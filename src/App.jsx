c4c3562c0ae88e128d59438b0ae1b1f4a906bf65
Salahed23 <khadirisalaheddine7@gmail.com>
2026-01-13 14:08:23 +0100

    Update project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { PanierProvider } from "./context/PanierContext";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { PanierProvider } from "./Context/PanierContext";
 import Home from "./Pages/Home";
 import Catalogue from "./Pages/Catalogue";
 import Inscription from "./Pages/Inscription";
@@ -12,12 +17,12 @@ function App() {
     <PanierProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Navigate to="/home" replace />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/catalogue" element={<Catalogue />} />
-          <Route path="/inscription" element={<Inscription />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/panier" element={<Panier />} />
+          <Route path='/' element={<Navigate to='/home' replace />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/catalogue' element={<Catalogue />} />
+          <Route path='/inscription' element={<Inscription />} />
+          <Route path='/Login' element={<Login />} />
+          <Route path='/panier' element={<Panier />} />
         </Routes>
       </Router>
     </PanierProvider>
--- src/Pages/Panier.jsx
+++ src/Pages/Panier.jsx
@@ -1,33 +1,38 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { PanierContext } from "../context/PanierContext";
+import { PanierContext } from "../Context/PanierContext";
 
 export default function Panier() {
-    const { panier, supprimerDuPanier, viderPanier } = useContext(PanierContext);
+  const { panier, supprimerDuPanier, viderPanier } = useContext(PanierContext);
 
-    return (
-        <div style={{ padding: "20px" }}>
-            <h1>🛒 Mon Panier</h1>
-            {panier.length === 0 ? (
-                <p>Votre panier est vide</p>
-            ) : (
-                <>
-                    <ul>
-                        {panier.map((item, index) => (
-                            <li key={item.id + "-" + index}>
-                                {item.name} - {item.prix}€
-                                <button onClick={() => supprimerDuPanier(item.id)} style={{ marginLeft: "10px" }}>
-                                    Supprimer
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
-                    <button onClick={viderPanier} style={{ marginTop: "10px" }}>Vider le panier</button>
-                </>
-            )}
-            <div style={{ marginTop: "20px" }}>
-                <Link to="/catalogue">Retour au catalogue</Link>
-            </div>
-        </div>
-    );
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>🛒 Mon Panier</h1>
+      {panier.length === 0 ? (
+        <p>Votre panier est vide</p>
+      ) : (
+        <>
+          <ul>
+            {panier.map((item, index) => (
+              <li key={item.id + "-" + index}>
+                {item.name} - {item.prix}€
+                <button
+                  onClick={() => supprimerDuPanier(item.id)}
+                  style={{ marginLeft: "10px" }}
+                >
+                  Supprimer
+                </button>
+              </li>
+            ))}
+          </ul>
+          <button onClick={viderPanier} style={{ marginTop: "10px" }}>
+            Vider le panier
+          </button>
+        </>
+      )}
+      <div style={{ marginTop: "20px" }}>
+        <Link to='/catalogue'>Retour au catalogue</Link>
+      </div>
+    </div>
+  );
 }
